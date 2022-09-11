ca60ad07361987001bf50533df37954582bb91cc
Mac <kgomokoena16@yahoo.com>
2022-09-11 13:42:57 +0200

    first commit

---
+++ theConsole.js
@@ -0,0 +1,9 @@
+//a) Console log
+
+var firstName = "Kgotso";
+var lasttName = "Mokoena";
+var institution = "University Of Johannesburg";
+var githubName = "kgomokoena";
+
+console.log("My name is " + firstName +" "+lasttName+" a student from " + institution+" and my github username is "+githubName);
+
